cb4da3aa000eda48653c11f70e1681f8c96f16b2
kaolnwza <kaow_jijuy999@hotmail.com>
2022-02-13 18:06:50 +0700

    add cors in backend

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,17 @@ import (
 	"daeng-market/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+		AllowMethods:     "GET, POST, PUT, DELETE",
+	}))
 
 	controllers.Route(app)
 
