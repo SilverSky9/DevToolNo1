8f7d4e219e593362a2d5b44b30f6f05984df8424
silversky9 <nopphadon.phanwong@gmail.com>
2022-04-15 15:14:48 +0700

    feat: add command for cd

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo "Hello bash!"
+def remote = [:]
+remote.name = "root"
+remote.host = '159.223.45.216'
+remote.user = 'root'
+remote.password = 'xitgmLwmp12q'
+remote.allowAnyHosts = true
+
+git clone https://github.com/SilverSky9/DevToolNo1.git
