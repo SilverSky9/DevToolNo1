0e70f757160b203eb64d19c126213766c624ea94
silversky9 <nopphadon.phanwong@gmail.com>
2022-04-15 15:20:21 +0700

    feat: Test connecting to server

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-def remote = [:]
-remote.name = "root"
-remote.host = '159.223.45.216'
-remote.user = 'root'
-remote.password = 'xitgmLwmp12q'
-remote.allowAnyHosts = true
-
-git clone https://github.com/SilverSky9/DevToolNo1.git
+echo 'Hello sh'
